533021907fdfc841c0eb509831ea4f6c3be635ae
m1shaas <blackbirdfa4@gmail.com>
2024-11-26 21:33:21 +0100

    Delete Ship.cc

--- Ship.cc
+++
@@ -1,4 +0,0 @@
-#include "Ship.h"
-
-Ship::Ship(int number, char letter, int size, const string& name, bool horizontal)
-    : number(number), letter(letter), size(size), name(name), horizontal(horizontal) {}
